36ff72d1a87b981446b9a5950fb0ddd004c903d4
Michael Hatherly <michaelhatherly@gmail.com>
2014-04-21 10:58:01 +0200

    Formatting changes.

--- src/build_matrix.m
+++ src/build_matrix.m
@@ -1,19 +1,22 @@
 function [A] = build_matrix(D, h, k, n)
-    % Construct the coefficient matrix "A" to solve Crank-Nicolson.
-    % 
-    % D := Diffusion constant.
-    % h := Step length for x.
-    % k := Step length for t.
-    % n := Size of the matrix.
-    
-    % Values for diagonal entries.
-    r = (D * k) / h^2;
-    R = r * ones(n, 1);
-    
-    % Fill sparse matrix "A" with diagonal entries.
-    A = spdiags([R -2*R R], -1:1, n, n);
-    
-    % Neumann conditions at endpoints "a" and "b".
-    A(1, 2) = 2*r; 
-    A(end, end-1) = 2*r;  
+%BUILD_MATRIX Construct matrix "A" to solve Crank-Nicolson model.
+% 
+%   D := Diffusion constant.
+%   h := Step length for x.
+%   k := Step length for t.
+%   n := Size of the matrix.
+%
+%   A := Resulting tridiagonal matrix.
+
+% Values for diagonal entries.
+r = (D * k) / h^2;
+R = r * ones(n, 1);
+
+% Fill sparse matrix "A" with diagonal entries.
+A = spdiags([R -2*R R], -1:1, n, n);
+
+% Neumann conditions at endpoints "a" and "b".
+A(1, 2) = 2*r; 
+A(end, end-1) = 2*r;
+
 end
